16651cad528758ca6e9cc376299a47cc6ec80988
deanchen <antlet2004@gmail.com>
2019-05-16 10:32:59 +0800

    [install] enzyme; add test for index page

--- test.test.js
+++ test.test.js
@@ -1,6 +1,12 @@
 //@flow
+import {shallow} from 'enzyme'
+import React from 'react'
+
+import Index from './pages/index.js' 
 
 describe('test', () => {
 	it('test', () => {
+		const index		= shallow(<Index/>)
+		console.log(index.debug())
 	})
 })
